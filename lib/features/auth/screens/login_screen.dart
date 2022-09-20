e4773de08e00382968bdfb07100e1f86173d8b28
Nikhil <nik.05.jindal@gmail.com>
2022-09-20 11:03:15 +0530

    Created routes

---
+++ lib/features/auth/screens/login_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  static const routeName = '/login-screen';
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Enter your phone number'),
+
+      ),
+    );
+  }
+}
